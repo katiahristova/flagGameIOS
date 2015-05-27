a0cf1cc190b0445596b5e17d29f6194c8ade2ecf
Katia Hristova <katiahristova@gmail.com>
2015-05-27 10:13:36 -0400

    Initial Commit

---
+++ flagsGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flagsGame
+//
+//  Created by Katia Hristova on 5/27/15.
+//  Copyright (c) 2015 ThraceCode Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
