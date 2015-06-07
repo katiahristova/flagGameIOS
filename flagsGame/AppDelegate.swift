162b1210d2a38d22a583d4ff974b8373181f4ef7
Sami <YOUR EMAIL ADDRESSsmail.ali@mail.mail.citytech,cuny.edu>
2015-06-07 11:46:01 -0400

    First push

--- flagsGame/AppDelegate.swift
+++ flagsGame/AppDelegate.swift
@@ -47,14 +47,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    
+    //ERRORS HERE!
 //    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
 //        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
 //    }
     
-    
-    
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool{
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
         return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 }
