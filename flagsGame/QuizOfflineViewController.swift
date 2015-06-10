4488000830f8779703ec65f9a01ef746ecaca9f5
Katia Hristova <katiahristova@gmail.com>
2015-06-10 19:15:09 -0400

    Guess buttons functionality implemented. Next button shows up when a correct answer is give and loads the next question.

--- flagsGame/QuizOfflineViewController.swift
+++ flagsGame/QuizOfflineViewController.swift
@@ -9,62 +9,37 @@
 import UIKit
 
 class QuizOfflineViewController: UIViewController {
-    var arrayAllCountries = [""]
+    var buttonsArray:[UIButton] = []
+    var data = DataArrays()
+    var arrayAllCountries:[String] = []
+    var questionCountries:[String] = []
+    var numberOfGuesses = 4;
+    var correctAnswer = ""
     var index = 0
     @IBOutlet weak var flagView: UIImageView!
+    @IBOutlet weak var buttonNext: UIButton!
     
-    @IBAction func nextButtonClick(sender: UIButton) {
-        
-        flagView.image = UIImage(named:arrayAllCountries[index])
-        if (index < arrayAllCountries.count-1)
-        {
-            index++ }
-        else
-        {
-            index=0 }
-    }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        var data = DataArrays()
+        buttonNext.hidden = true
         
         //Set regions for the game
         var regions = ["Africa", "Asia", "Europe"]
-        var numberOfGuesses = 6;
         
         //Get flags filename list according to regions selected
         arrayAllCountries = data.getCountriesList(regions)
         
-        var questionCountries = data.getCountriesForQuestion(numberOfGuesses, countries: arrayAllCountries)
-        var correctAnswer = questionCountries[data.getRandomIndex(questionCountries)]
+        questionCountries = data.getCountriesForQuestion(numberOfGuesses, countries: arrayAllCountries)
+        correctAnswer = questionCountries[data.getRandomIndex(questionCountries)]
         println(correctAnswer)
+        
+        flagView.image = UIImage(named:correctAnswer)
         
         
         // Do any additional setup after loading the view.
         
-        var buttonYplacement = 400
-        var nextLabel = 0
-        for var i = 0; i<numberOfGuesses/2; i++
-        {
-            let buttonLeft   = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-            buttonLeft.frame = CGRectMake(40, CGFloat(buttonYplacement), 120, 50)
-            buttonLeft.backgroundColor = UIColor.yellowColor()
-            buttonLeft.setTitle(questionCountries[nextLabel], forState: UIControlState.Normal)
-            nextLabel++
-            buttonLeft.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
-            self.view.addSubview(buttonLeft)
-        
-            let buttonRight   = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-            buttonRight.frame = CGRectMake(200, CGFloat(buttonYplacement), 120, 50)
-            buttonRight.backgroundColor = UIColor.yellowColor()
-            buttonRight.setTitle(questionCountries[nextLabel], forState: UIControlState.Normal)
-            nextLabel++
-            buttonRight.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
-            self.view.addSubview(buttonRight)
-            
-            buttonYplacement += 60
-        }
+        createGuessButtons()
         
     }
 
@@ -77,7 +52,62 @@ class QuizOfflineViewController: UIViewController {
 
     func buttonAction(sender:UIButton!)
     {
+        if (sender.titleLabel?.text == correctAnswer)
+        { buttonNext.hidden = false }
+        println(sender.titleLabel?.text)
         println("Button tapped")
+        
+    }
+    
+    @IBAction func nextButtonClick(sender: UIButton) {
+        questionCountries = data.getCountriesForQuestion(numberOfGuesses, countries: arrayAllCountries)
+        correctAnswer = questionCountries[data.getRandomIndex(questionCountries)]
+        println(correctAnswer)
+        
+        flagView.image = UIImage(named:correctAnswer)
+        for var i = 0; i<numberOfGuesses; i++
+        {
+            buttonsArray[i].setTitle(questionCountries[i], forState: UIControlState.Normal)
+        }
+        buttonNext.hidden = true
+        
+        //Iterate through flags when Next button is clicked
+        /* flagView.image = UIImage(named:arrayAllCountries[index])
+        
+         if (index < arrayAllCountries.count-1)
+        {
+            index++ }
+        else
+        {
+            index=0 }*/
+    }
+
+    func createGuessButtons()
+    {
+        var buttonYplacement = 400
+        var nextLabel = 0
+        for var i = 0; i<numberOfGuesses/2; i++
+        {
+            let buttonLeft   = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+            buttonLeft.frame = CGRectMake(40, CGFloat(buttonYplacement), 120, 50)
+            buttonLeft.backgroundColor = UIColor.yellowColor()
+            buttonLeft.setTitle(questionCountries[nextLabel], forState: UIControlState.Normal)
+            nextLabel++
+            buttonLeft.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+            self.view.addSubview(buttonLeft)
+            buttonsArray.append(buttonLeft)
+            
+            let buttonRight   = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+            buttonRight.frame = CGRectMake(200, CGFloat(buttonYplacement), 120, 50)
+            buttonRight.backgroundColor = UIColor.yellowColor()
+            buttonRight.setTitle(questionCountries[nextLabel], forState: UIControlState.Normal)
+            nextLabel++
+            buttonRight.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+            self.view.addSubview(buttonRight)
+            buttonsArray.append(buttonRight)
+            
+            buttonYplacement += 60
+        }
     }
     
     /*
